035b93b4effe9e0b3a2c66aab19da56cc5aaee0c
Artem <vavilzidane007@mail.ru>
2022-01-18 01:31:21 +0400

    first task

---
+++ src/main/java/homework3/SetWords.java
@@ -0,0 +1,26 @@
+package homework3;
+
+import java.util.*;
+
+public class SetWords {
+    public static void main (String [] args) {
+
+        String [] set = { "dog", "cat", "rabbit", "delfin", "bird", "camel", "cat", "elephant", "dog", "cow",
+                "delfin", "chicken", "dog"};
+
+        List<String> list  = new ArrayList<>(Arrays.asList(set));
+        System.out.println("Список животных:" +list );
+
+        Set <String> uniqueWord = new TreeSet<String>();
+        uniqueWord.addAll(Arrays.asList(set));
+        System.out.println("Список уникальных животных: " + uniqueWord);
+
+        Map <String, Integer> map = new HashMap<>();
+        for (String word : set) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+        System.out.println("");
+
+        }
+    }
