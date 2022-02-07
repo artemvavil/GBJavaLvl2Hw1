d7644fe2b98c4b22b32a7bee2b5a56bc0642fe9b
Artem <vavilzidane007@mail.ru>
2022-02-08 01:05:23 +0400

    fnl

--- src/main/java/homework7/MyServer.java
+++ src/main/java/homework7/MyServer.java
@@ -36,6 +36,17 @@ public class MyServer {
         }
     }
 
+    public synchronized void sendMsgToClient(ClientHandler from, String nickTo, String msg) {
+        for (ClientHandler o : clients) {
+            if (o.getName().equals(nickTo)) {
+                o.sendMsg("от " + from.getName() + ": " + msg);
+                from.sendMsg("клиенту " + nickTo + ": " + msg);
+                return;
+            }
+        }
+        from.sendMsg("Участника с ником " + nickTo + " нет в чат-комнате");
+    }
+
     public synchronized boolean isNickBusy(String nick) {
         for (ClientHandler o : clients) {
             if (o.getName().equals(nick)) {
@@ -51,11 +62,22 @@ public class MyServer {
         }
     }
 
+    public synchronized void broadcastClientsList() {
+        StringBuilder sb = new StringBuilder("/clients ");
+        for (ClientHandler o : clients) {
+            sb.append(o.getName() + " ");
+        }
+        broadcastMsg(sb.toString());
+    }
+
     public synchronized void unsubscribe(ClientHandler o) {
         clients.remove(o);
+        broadcastClientsList();
     }
 
     public synchronized void subscribe(ClientHandler o) {
         clients.add(o);
+        broadcastClientsList();
     }
 }
+
